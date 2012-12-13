142b690833c7b1b2f881def9121f9524c1ca6c65
Michael Heu <mheu@clickbooq.com>
2012-12-13 08:54:13 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FolioApp::Application.config.secret_token = '69f89bf1bdb1e87af888d62f275168464f79b402ecf60d0f1b2a603cae8fcc52a29293f8a745e13f97909a08339ece3dacbb16cf55a491b1b0017f12c950de90'

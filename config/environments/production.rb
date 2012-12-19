73428ce05ac73be84eb34f1bb56518cc41d3104a
Michael Heu <mheu@clickbooq.com>
2012-12-19 10:29:08 -0500

    changed production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ FolioApp::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
   
   # Prevent initializing your application and connecting to the database for Heroku
   config.assets.initialize_on_precompile = false

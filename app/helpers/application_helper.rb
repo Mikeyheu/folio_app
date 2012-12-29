a2f54f119cfb81f1b2850ab874a2ac8b0a998c4f
Michael Heu <mheu@clickbooq.com>
2012-12-29 16:56:31 -0500

    added icon helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+	def icon(class_name)
+		case class_name
+      when "Page" then "content-page"
+      when "Link" then "link"
+      when "Folder" then "folder"
+      else "gallery"
+	  end
+	end
 end

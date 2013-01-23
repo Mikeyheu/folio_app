623918362395d730e703a6576a9c2cf589daf7fb
Michael Heu <mheu@clickbooq.com>
2013-01-23 11:18:23 -0500

    added gallery nav item association

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,8 +1,9 @@
 class Gallery < ActiveRecord::Base
   belongs_to :site
+  has_one :nav_item, :as =>:navable, :dependent => :destroy
   has_many :gallery_assignments, :dependent => :destroy
   has_many :images, :through => :gallery_assignments, :order => 'gallery_assignments.position'
-
+  
   extend FriendlyId
   friendly_id :name, use: :slugged
 
